41703ea2cdb30cfcdf3053e3b5e04ce1bb9a9b98
Gourav kumar <gouravkumar6097@gmail.com>
2025-10-02 19:41:02 +0530

    Login page

---
+++ Login.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Login{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int password;
+        String name;
+
+        System.out.println("Enter your password");
+        password = sc.nextInt();
+        System.out.println("Enter your name");
+         name =sc.nextLine();
+
+        if(name=="Gourav Kumar"){
+            if(password==123456){
+                System.out.println("Login successfully");
+            }
+        }
+
+
+    }
+}
